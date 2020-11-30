0f26659097ce24aee536337ea0924c82397a98d0
a0k00xa <ambreen.khan@walmart.com>
2020-11-29 20:00:52 -0500

    gathsby-blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,36 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "Tech Journal",
+    description: "Everything I like to learn & explore.",
+    author: "Ambreen Khan",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    // "gatsby-plugin-sharp",
+    // {
+    //   resolve: "gatsby-transformer-remark",
+    //   options: {
+    //     plugins: [
+    //       {
+    //         resolve: "gatsby-remark-images",
+    //         options: {
+    //           maxWidth: 750,
+    //           linkImagesToOriginal: false,
+    //         },
+    //       },
+    //     ],
+    //   },
+    // },
+],
+  
 }
