866745990ebddcb6c8b855d585bf5c95565d20a5
Ambreen Khan <Ambreen.Khan@walmart.com>
2020-11-29 21:04:48 -0500

    update homepage q exit

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,27 @@ const Index = () => {
     <Layout>
       <h1>Welcome to Tech Journal 📔</h1>
       <h2>A Knowledge Centre for Software Testing Professionals</h2>
+      <div>
+        <p>
+        QA’s role is changing in the current DevOps culture. Processes are moving towards faster testing and build deployment. If you want to embrace new technological changes, step ahead and learn new skills. Some skills that are worthwhile for your testing career are:</p>
+        <ul>
+          <li>Programming knowledge</li>
+          <li>Basic networking knowledge</li>
+          <li>Basic Unix/Shell scripting</li>
+          <li>Continuos Testing</li>
+          <li>Continuous Integration</li>
+          <li>Continuous Delivery/Continuous Deployment</li>
+          <li>Performance testing</li>
+          <li>Operations and Resilience Testing</li>
+          <li>Knowledge of Containers, Docker, Kubernetes</li>
+          <li>Application Monitoring tools such as Splunk, Dynatrace</li>
+          <li>Clouds services</li>
+        </ul>
+        <p>Sharpen your tools, skills and your mind to succeed. Try out new things. Just remember to take the time to do so!</p>
+      </div>
+
+        
+
     </Layout>
   )
 }
